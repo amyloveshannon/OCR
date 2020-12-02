99c6d0bd4c2aa9c9e2cd4528bce926b2070c5c78
amyloveshannon <amyloveshannon@gmail.com>
2020-12-02 17:06:42 +0800

    add comment

--- capturePic.py
+++ capturePic.py
@@ -2,14 +2,14 @@ import cv2
 import time
 import datetime
  
-cap=cv2.VideoCapture(0)#選擇第1隻攝影機
+cap=cv2.VideoCapture(0) #0表示選擇電腦第1台攝影機
 today = datetime.datetime.now()
 i=0
 while(1):
-    
-    ret ,frame = cap.read()#從攝影機擷取一張影像
-    k=cv2.waitKey(1)
-    print(k)
+    time.sleep(2)#讓攝影機能調整曝光、焦距等，提高照片品質，等2秒之後再拍
+    ret ,frame = cap.read() #從攝影機擷取一張影像，若成功取得影像ret得到True，否則為False。影像資料存在frame變數中。
+    k=cv2.waitKey(1) #等待1毫秒看看這時間內使用者是否按下鍵盤'Q:81(關閉)'、'S:83(儲存)'
+    print(k) #Q:81(關閉)、S:83(儲存)
     
     if k==81:
         break
@@ -24,10 +24,10 @@ while(1):
             imgMonth = str(today.month)
             imgDay = str(today.day)
             
-        cv2.imwrite('C:/Users/Egg/Desktop/RecogDigit/DigitImg/' + imgMonth + imgDay + "-" + str(i) + '.jpg',frame)#將擷取的影像存入
+        cv2.imwrite('D:/數字辨識/Github/OCsR/DigitImg' + imgMonth + imgDay + "-" + str(i) + '.jpg',frame)#將擷取的影像存入
         i+=1
 
-    cv2.imshow("capture", frame)#顯示圖片
+    cv2.imshow('captured_pic', frame)#顯示圖片
     #time.sleep(1)#每2(s)拍一次
 
 cap.release()#釋放攝影機
